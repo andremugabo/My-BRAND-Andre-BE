e0ec6fc51b6eb719f11748fea929d17109f9978c
andremugabo <andremugabo@yahoo.fr>
2024-03-18 19:03:33 +0200

    access control handle error

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  next();
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next(); 
 });
 
 
