542fa4f792741321e4383c8e621c1e4ccb156205
andremugabo <andremugabo@yahoo.fr>
2024-03-18 17:47:28 +0200

    access control to github

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,6 @@ const connection_url: string = process.env.DB_URL!;
 // CORS middleware
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://andremugabo.github.io');
-  // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   next();
