65a86da3aefcf6b4a42872beb7749e3f256a8723
andremugabo <andremugabo@yahoo.fr>
2024-03-18 13:33:00 +0200

    fixe user id type

--- src/models/blogModel.ts
+++ src/models/blogModel.ts
@@ -1,58 +1,59 @@
-import mongoose,{Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 import joi from 'joi';
 
-export interface IBlog extends Document{
-    userId :string;
-    blogCategoryId:string;
+export interface IBlog extends Document {
+    userId: string;
+    blogCategoryId: string;
     blogTitle: string;
-    blogDescription:string;
-    blogContent:string;
+    blogDescription: string;
+    blogContent: string;
     blogImg: string;
     blogDate: Date;
 }
 
-const blogSchema : Schema = new Schema({
-    userId:{
-        type:String,
-        required:true,
+const blogSchema: Schema = new Schema({
+    userId: {
+        type: String,
+        required: true,
     },
-    blogCategoryId:{
-        type:String,
-        required:true
+    blogCategoryId: {
+        type: String,
+        required: true
     },
-    blogTitle:{
-        type:String,
-        required:[true,'Blog title is required'],
+    blogTitle: {
+        type: String,
+        required: [true, 'Blog title is required'],
     },
-    blogDescription:{
-        type:String,
-        required:[true,'Blog description is required'],
+    blogDescription: {
+        type: String,
+        required: [true, 'Blog description is required'],
     },
-    blogContent:{
-        type:String,
-        required:[true,'Blog content is required'],
+    blogContent: {
+        type: String,
+        required: [true, 'Blog content is required'],
     },
-    blogImg:{
-        type:String,
-        required:[true,'Blog image is required'],
+    blogImg: {
+        type: String,
+        required: [true, 'Blog image is required'],
     },
-    blogDate:{
-        type:Date,
-        default: Date
+    blogDate: {
+        type: Date,
+        default: Date.now
     }
 });
 
-const Blog = mongoose.model<IBlog>('Blog',blogSchema);
+const Blog = mongoose.model<IBlog>('Blog', blogSchema);
 export default Blog;
-export const joinBlogValidation = (blogEntry : IBlog)=>{
+
+export const joinBlogValidation = (blogEntry: IBlog) => {
     const schema = joi.object({
-        userId : joi.string().required(),
-        blogTitle:joi.string().required(),
+        userId: joi.string().required(),
+        blogTitle: joi.string().required(),
         blogCategoryId: joi.string().required(),
         blogDescription: joi.string().min(10).max(500).required(),
-        blogContent : joi.string().min(10).max(10000).required(), 
-        blogImg: joi.string().required(),  
-        blogDate: joi.date() 
+        blogContent: joi.string().min(10).max(10000).required(),
+        blogImg: joi.string().required(),
+        blogDate: joi.date()
     });
     return schema.validate(blogEntry);
 }
