eec01275d341daf3dfe4ee5c6874d1e2a14207d9
andremugabo <andremugabo@yahoo.fr>
2024-03-21 12:03:56 +0200

    msg contact

--- dist/controller/contactMsgController.js
+++ dist/controller/contactMsgController.js
@@ -85,19 +85,19 @@ const patchContactMsgById = (req, res) => __awaiter(void 0, void 0, void 0, func
         const checkUser = yield (0, verifyToken_1.getUser)(req.myAppToken);
         if (checkUser && checkUser.isAdmin) {
             const { id } = req.params;
-            const readMsg = yield contactMsgModel_1.default.findByIdAndUpdate({ _id: id }, { read: '1' }, { new: true });
+            const readMsg = yield contactMsgModel_1.default.findByIdAndUpdate(id, { read: '1' }, { new: true });
             if (!readMsg) {
-                return res.status(404).json({ message: `Cannot find a message with  ID ${id}` });
+                return res.status(404).json({ message: `Cannot find a message with ID ${id}` });
             }
-            res.status(200).json(readMsg);
+            return res.status(200).json({ readMsg, status: 200 });
         }
         else {
-            res.status(401).json({ message: "YOU ARE NOT AUTHORIZED TO READ MESSAGE" });
+            return res.status(401).json({ message: "YOU ARE NOT AUTHORIZED TO READ MESSAGE" });
         }
     }
     catch (error) {
         console.log(error.message);
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 });
 exports.patchContactMsgById = patchContactMsgById;
